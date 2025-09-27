c64422f3130cc6510b0b6aeac7fcfcfd77330ac8
MliliGenes <saad.elm.77@gmail.com>
2025-09-27 10:01:48 +0100

    Array json type

--- include/TrpJsonArray.hpp
+++ include/TrpJsonArray.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "TrpJsonValue.hpp"
+#include <vector>
 
 #ifndef TRPARRAY_HPP
 #define TRPARRAY_HPP
 
 class trpJsonArray : public ITrpJsonValue {
-
-};
+    private:
+        vector<ITrpJsonValue*> m_value;
+}; 
 
 #endif // TRPARRAY_HPP

9b682ccdbbc4f59cdb76e73b6069fe836d7ae1d7
MliliGenes <saad.elm.77@gmail.com>
2025-09-27 14:51:12 +0100

    i am way to high wo wo wooo

--- include/TrpJsonLexer.hpp
+++ include/TrpJsonLexer.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 #ifndef TRPLEXER_HPP
 #define TRPLEXER_HPP
@@ -22,15 +23,39 @@ enum TrpTokenType {
     T_ERROR
 };
 
+struct token {
+    TrpTokenType type;
+    std::string value;  // only used for strings, numbers, errors
+    size_t line;        // 0-based line number
+    size_t col;         // 0-based column number
+};
+
 class TrpJsonLexer {
     private:
         bool opened;
+        std::vector<std::string> lines;
         std::ifstream json_file;
         std::string file_name;
         size_t line, col;
 
+        void skipWhitespace();
+        char peekChar() const;
+        char getChar();
+        TrpToken readString();
+        TrpToken readNumber();
+        TrpToken readLiteral();
+
+
     public:
         TrpJsonLexer(std::string file_name);
+        ~TrpJsonLexer();
+        TrpToken getNextToken();
+        bool isOpen() const { return opened; }
 };
 
 #endif // TRPLEXER_HPP
+
+
+// value  → object | array | string | number | true | false | null
+// object → '{' (string ':' value (',' string ':' value)*)? '}'
+// array  → '[' (value (',' value)*)? ']'
